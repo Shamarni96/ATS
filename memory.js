4f6b3d0a02a470df484ea5996fc65b3cb817e676
shamarni mcfarlane <Smcfarlane926@gmail.com>
2020-06-08 17:49:49 +0100

    Update v1

--- memory.js
+++ memory.js
@@ -1,32 +1,47 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasCardFlipped = false;
+let lockGame = false;
 let firstCard, secondCard;
 
+
 function flipCard() {
+  if (lockGame) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
+  
 
   if (!hasCardFlipped ) {
     // first click
     hasCardFlipped  = true;
     firstCard = this;
+
   } else {
     // second click
     hasCardFlipped  = false;
     secondCard = this;
 
+
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
       // if it matches
       firstCard.removeEventListener('click', flipCard);
       secondCard.removeEventListener('click', flipCard);
+      resetLock()
     } else {
+      lockGame = true;
       // if it doesn't match
       setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetLock()
       }, 1500);
     }
   }
+}
+
+function resetLock() {
+  [hasCardFlipped, lockGame] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 (function shuffleCards() {
